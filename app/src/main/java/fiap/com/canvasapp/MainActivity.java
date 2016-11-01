e48e15b73c8b12c74c409f76297c7d9942f269ce
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-10-31 23:00:44 -0200

    Just creating something as an example

--- app/src/main/java/fiap/com/canvasapp/MainActivity.java
+++ app/src/main/java/fiap/com/canvasapp/MainActivity.java
@@ -1,13 +1,42 @@
 package fiap.com.canvasapp;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView img_face;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img_face = (ImageView) findViewById(R.id.img_face);
+    }
+
+    public void drawSimple() {
+        Bitmap bmp = Bitmap.createBitmap(200, 200, Bitmap.Config.ARGB_8888);
+        // Bitmap.Config.ARGB_8888 16 colors by pixel
+
+        Canvas c = new Canvas(bmp);
+        Paint p = new Paint();
+        p.setColor(Color.YELLOW);
+
+        Paint p2 = new Paint();
+        p2.setColor(Color.BLUE);
+
+        c.drawCircle(100, 100, 60, p);
+        c.drawLine(80, 140, 120, 140, p2);
+
+        c.drawCircle(80,80,10, p);
+        c.drawCircle(120,80,10, p2);
+
+        img_face.setImageBitmap(bmp);
     }
 }
